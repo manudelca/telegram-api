040afe25ad95f6a3269614f2be594a036003d23c
Santiago Alvarez Juliá <santiago.alvarezjulia@gmail.com>
2021-02-13 14:49:10 -0300

    add configure_heroku_test script

--- scripts/configure_heroku_test.sh
+++ scripts/configure_heroku_test.sh
@@ -5,9 +5,9 @@ set -e
 
 # heroku login
 
-APP_NAME=webapi-volantis
+APP_NAME=webapi-volantis-test
 
 heroku apps:create $APP_NAME
 heroku addons:create heroku-postgresql:hobby-dev --app $APP_NAME
 heroku config:set RACK_ENV=production --app $APP_NAME
-heroku drains:add $SUMOLOGIC_SOURCE --app $APP_NAME
+heroku drains:add $SUMOLOGIC_SOURCE_TEST --app $APP_NAME
