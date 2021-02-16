e683b12d33b40e5af34d544be90a1da5d1623b8a
manudelca <manueldelcarril@gmail.com>
2021-02-16 11:33:35 -0300

    client guarda las peliculas vistas

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,5 +6,14 @@ class Client
     @email = email
     @username = username
     @id = id
+    @content_seen = Set.new
+  end
+
+  def sees_content(content)
+    @content_seen.add(content)
+  end
+
+  def saw_content?(content)
+    @content_seen.include?(content)
   end
 end
---
+++ spec/app/models/client_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Client do
+  let(:client) { described_class.new('juan@test.com', 'juan') }
+
+  it 'should be able to mark movies as seen' do
+    genre = Genre.new('Drama')
+    movie_id = 0
+    movie = Movie.new('Titanic', 'ATP', 190, genre, 'USA', 'James Cameron', '2020-01-01', 'Leonardo Di Caprio', 'Kate', movie_id)
+    client.sees_content(movie)
+
+    expect(client.saw_content?(movie)).to eq(true)
+  end
+end
