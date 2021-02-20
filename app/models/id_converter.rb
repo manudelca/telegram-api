04c78e03e0893cf61e96312bca4b3e5ec0e1200c
EugeniaFranchi <mfranchi@fi.uba.ar>
2021-02-20 20:31:46 -0300

    id convertor devuelve tvshow repo

--- app/models/id_converter.rb
+++ app/models/id_converter.rb
@@ -1,7 +1,15 @@
 require_relative '../persistence/repositories/movie_repo'
 
 class IdConverter
-  def get_repo(_id)
-    Persistence::Repositories::MovieRepo.new(DB)
+  attr_accessor :repos
+
+  def initialize
+    @repos = {'00' => Persistence::Repositories::MovieRepo.new(DB),
+              '01' => Persistence::Repositories::TvShowRepo.new(DB)}
+  end
+
+  def get_repo(id)
+    repo_id = id[0, 2]
+    @repos[repo_id]
   end
 end
--- spec/app/models/id_converter_spec.rb
+++ spec/app/models/id_converter_spec.rb
@@ -4,40 +4,40 @@ describe IdConverter do
   let(:converter) { described_class.new }
 
   it 'should return movie repo when first 2 digits are 00' do
-    id = 0o0123
+    id = '00123'
     repo = converter.get_repo(id)
     expect(repo).to be_instance_of(Persistence::Repositories::MovieRepo)
   end
 
-  xit 'should return tv show repo when first 2 digits are 01' do
-    id = 0o1123
+  it 'should return tv show repo when first 2 digits are 01' do
+    id = '01123'
     repo = converter.get_repo(id)
-    expect(repo).to be_instance_of(TvShowRepo)
+    expect(repo).to be_instance_of(Persistence::Repositories::TvShowRepo)
   end
 
   xit 'should return episode repo when first 2 digits are 02' do
-    id = 0o2123
+    id = '02123'
     repo = converter.get_repo(id)
     expect(repo).to be_instance_of(EpisodesRepo)
   end
 
   xit 'should parse id removing first 2 digits' do
-    id = 0o0123
+    id = '00123'
     expect(converter.parse_id(id)).to eq(123)
   end
 
   xit 'should add 00 to movie created' do
-    id = 1123
+    id = '1123'
     expect(converter.parse_movie_id(id)).to eq(0o01123)
   end
 
   xit 'should add 01 to tv show created' do
-    id = 1123
+    id = '1123'
     expect(converter.parse_movie_id(id)).to eq(0o11123)
   end
 
   xit 'should add 02 to episode created' do
-    id = 1123
+    id = '1123'
     expect(converter.parse_movie_id(id)).to eq(0o21123)
   end
 end
